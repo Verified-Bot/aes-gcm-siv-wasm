21af6a1a4247444b6925787a183234e1f0e5b267
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-12-14 02:31:06 -0600

    fix deploy script

--- build.sh
+++ build.sh
@@ -3,12 +3,10 @@ wasm-pack build
 
 # remove last line from package.json fine
 LC=`wc -l < ./pkg/package.json`
-LC=$(($LC - 2 ))
+LC=$(($LC - 1))
 head -n $LC ./pkg/package.json > ./pkg/package_n.json
 rm ./pkg/package.json
 mv ./pkg/package_n.json ./pkg/package.json
-
-cat ./pkg/package.json
 
 # add module type to pakcage.json
 echo '  "type": "module",' >> pkg/package.json
