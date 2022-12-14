64699bd4d2c673b54525d0b39f1971fd908cb626
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-12-14 02:27:04 -0600

    debug

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ head -n $LC ./pkg/package.json > ./pkg/package_n.json
 rm ./pkg/package.json
 mv ./pkg/package_n.json ./pkg/package.json
 
+cat ./pkg/package.json
+
 # add module type to pakcage.json
 echo '  "type": "module",' >> pkg/package.json
 echo "}" >> pkg/package.json
